4c8be22fc63b3ec7e6953e8fafcc14efe4662049
YashPalav-26 <yashpalav100@gmail.com>
2026-03-08 15:00:00 +0530

    redundant text removed and removed the unused import react

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LANGUAGE_VERSIONS } from "../constants";
+import { LANGUAGE_VERSIONS } from "../constants/constants";
 
 const ONECOMPILER_API = "/api/execute"; 
 
