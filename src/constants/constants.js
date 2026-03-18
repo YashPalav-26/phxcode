15bd32e8d457da6dee68b60f0f5fa34804b95f07
YashPalav-26 <yashpalav100@gmail.com>
2026-03-18 12:42:14 +0530

    snippets modified

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -13,10 +13,10 @@ export const CODE_SNIPPETS = {
   javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Yash");\n`,
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Yash" });\n`,
   python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Yash")\n`,
-  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+  java: `\npublic class GreetYash {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello Yash");\n\t}\n}\n`,
   csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
+    'using System;\n\nnamespace GreetYash\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello Yash in C#");\n\t\t}\n\t}\n}\n',
   php: "<?php\n\n$name = 'Yash';\necho $name;\n",
-  c: `#include <stdio.h>\n\nint main() {\n    printf("Hello, World!\\n");\n    return 0;\n}\n`,
-  cpp: `#include <iostream>\n\nint main() {\n    std::cout << "Hello, World!" << std::endl;\n    return 0;\n}\n`,
+  c: `#include <stdio.h>\n\nint main() {\n    printf("Hello, Yash!\\n");\n    return 0;\n}\n`,
+  cpp: `#include <iostream>\n\nint main() {\n    std::cout << "Hello, Yash!" << std::endl;\n    return 0;\n}\n`,
 };
